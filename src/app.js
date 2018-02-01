7cbae24ac8d3a1a7699840ed236982c4c1fe864c
bobrobcpp <king.roob@gmail.com>
2018-02-01 18:54:58 +0000

    Initial set up

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+ReactDOM.render(<p> Memory Game </p>, document.getElementById('app'));
